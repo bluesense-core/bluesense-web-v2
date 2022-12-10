92bc1d2aa644ff21e730f2c7342ae03619213b66
Osamudiame Ruth Igbinoba <info@bluesense.co>
2022-12-10 12:34:53 +0100

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Home from './ui/Home';
 
 function App() {
-  return (
-    <div className="App">
-      Hello
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Header />
+
+            <Routes>
+                <Route path='/' element={<Home />} />
+            </Routes>
+
+            <Footer />
+        </BrowserRouter>
+    );
 }
 
 export default App;
