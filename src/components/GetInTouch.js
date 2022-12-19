f2b2597054e47703c9f4c6a172a0ef77e68f10fc
BlueSense <105964264+bluesense-core@users.noreply.github.com>
2022-12-19 10:54:28 +0000

    changes

--- src/components/GetInTouch.js
+++ src/components/GetInTouch.js
@@ -1,8 +1,28 @@
-import React from 'react';
-import './get-in-touch.css';
+import React from "react";
+import { Button, Container } from "react-bootstrap";
+import { BsArrowRight } from "react-icons/bs";
+import "./get-in-touch.css";
 
 const GetInTouch = () => {
-    return <div>GetInTouch</div>;
+	return (
+		<section id="getInTouch">
+			<Container className="d-flex flex-lg-row flex-column justify-content-between align-items-center">
+				<div className="detail">
+					<h1 className="mb-2">Get in touch</h1>
+					<p>
+						We’d love to speak to you and discuss how we can work together to
+						bring your ideas to life.
+					</p>
+				</div>
+				<div className="contact-us d-flex justify-content-center">
+					<Button className="px-5 py-4 d-flex justify-content-between align-items-center">
+						<span>Contact Us</span>
+						<BsArrowRight className="arrow" />
+					</Button>
+				</div>
+			</Container>
+		</section>
+	);
 };
 
 export default GetInTouch;
--- src/ui/Home.js
+++ src/ui/Home.js
@@ -1,8 +1,14 @@
 import React from "react";
+import GetInTouch from "../components/GetInTouch";
 import "./home.css";
 
 const Home = () => {
-	return <div id="home">Home</div>;
+	return (
+		<div id="home">
+			Home
+			<GetInTouch />
+		</div>
+	);
 };
 
 export default Home;
