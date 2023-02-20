afcba4e31ef0f346b424fe92d08d6cf873e5311f
Osamudiame Ruth Igbinoba <ruth.igbinoba12@gmail.com>
2023-02-20 08:02:55 +0100

    updates

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { hydrate, render } from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import ReactDOM from 'react-dom/client';
+// import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-);
+const rootElement = document.getElementById('root');
+if (rootElement.hasChildNodes()) {
+    hydrate(<App />, rootElement);
+} else {
+    render(<App />, rootElement);
+}
