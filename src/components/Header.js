5a0c670c6199a504d22c77ec749670112aaee112
Osamudiame Ruth Igbinoba <ruth.igbinoba12@gmail.com>
2023-08-11 08:22:18 +0100

    updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,6 @@ import {
     Nav,
     Navbar,
     NavDropdown,
-    Offcanvas,
 } from 'react-bootstrap';
 import './header.css';
 import Logo from '../assets/images/logo.svg';
@@ -15,24 +14,15 @@ import { NavHashLink, HashLink } from 'react-router-hash-link';
 
 const Header = () => {
     const navigate = useNavigate();
-    const [show, setShow] = useState(false);
-    const [dropdown, setDropdown] = useState(false);
-    const showDropdown = (e) => {
-        setDropdown(!show);
-    };
-    const hideDropdown = (e) => {
-        setDropdown(false);
-    };
+    const [menuOpen, setMenuOpen] = useState(false);
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-
-    const [active, setActive] = useState('/');
-    const handleActive = (e) => setActive(e);
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
 
     return (
         <header id='header'>
-            <Navbar className='bg-white ' expand='md'>
+            <Navbar className='bg-white ' expand=''>
                 <Container>
                     <Navbar.Brand href='/' className='py-2 d-flex'>
                         <Image
@@ -42,207 +32,107 @@ const Header = () => {
                         />
                     </Navbar.Brand>
 
-                    <Navbar.Toggle onClick={handleShow} />
+                    <Navbar.Toggle
+                        aria-controls='basic-navbar-nav'
+                        onClick={toggleMenu}>
+                        {menuOpen ? (
+                            <span className='btn-close'></span>
+                        ) : (
+                            <span className='navbar-toggler-icon' />
+                        )}
+                    </Navbar.Toggle>
 
-                    <Navbar.Offcanvas
-                        show={show}
-                        onHide={handleClose}
+                    <Navbar.Collapse
                         placement='start'
                         className='flex-md-grow-0'>
-                        <Offcanvas.Header className='py-3' closeButton>
-                            <Navbar.Brand href='/' className='d-flex'>
-                                <Image
-                                    src={Logo}
-                                    className='header-logo'
-                                    alt='BlueSense Logo'
-                                />
-                            </Navbar.Brand>
-                        </Offcanvas.Header>
+                        <Nav id='header-nav' className=''>
+                            <Nav.Item className=' pb-3 d-md-none d-block'>
+                                <NavLink to='/'>Home</NavLink>
+                            </Nav.Item>
+                            <Nav.Item className='pb-lg-4 pb-3'>
+                                <NavLink to='/about'>About</NavLink>
+                            </Nav.Item>
 
-                        <Offcanvas.Body>
-                            <Nav
-                                id='header-nav'
-                                activeKey={active}
-                                defaultActiveKey={active}
-                                onSelect={(e) => {
-                                    handleActive(e);
-                                }}
-                                className='justify-content-md-between align-items-md-center'>
-                                <Nav.Item className='px-lg-4 px-md-3 d-md-none d-block'>
-                                    <NavLink onClick={handleClose} to='/'>
-                                        Home
-                                    </NavLink>
-                                </Nav.Item>
-                                <Nav.Item className='px-lg-4 px-md-3'>
-                                    <NavLink onClick={handleClose} to='/about'>
-                                        About
-                                    </NavLink>
-                                </Nav.Item>
-
-                                <NavDropdown
-                                    title='Services'
-                                    onClick={(e) => navigate('/services')}
-                                    id='hover-dropdown'
-                                    className='px-lg-4 px-md-3 d-md-block d-none'
-                                    show={dropdown}
-                                    onMouseEnter={showDropdown}
-                                    onMouseLeave={hideDropdown}>
-                                    <NavDropdown.Item className=''>
-                                        <NavHashLink
-                                            smooth
-                                            className='d-block'
-                                            onClick={handleClose}
-                                            to='/services#learning'>
-                                            Learning
-                                        </NavHashLink>
-                                    </NavDropdown.Item>
-
-                                    <NavDropdown.Item className=''>
-                                        <NavHashLink
-                                            smooth
-                                            className='d-block'
-                                            onClick={handleClose}
-                                            to='/services#strategy'>
-                                            Strategy
-                                        </NavHashLink>
-                                    </NavDropdown.Item>
-
-                                    <NavDropdown.Item className=''>
-                                        <NavHashLink
-                                            smooth
-                                            className='d-block'
-                                            onClick={handleClose}
-                                            to='/services#advisory'>
-                                            Advisory / Coaching
-                                        </NavHashLink>
-                                    </NavDropdown.Item>
-
-                                    <NavDropdown.Item className=''>
-                                        <NavHashLink
-                                            smooth
-                                            className='d-block'
-                                            onClick={handleClose}
-                                            to='/services#fundraising'>
-                                            Fundraising
-                                        </NavHashLink>
-                                    </NavDropdown.Item>
-                                    <NavDropdown.Item className=''>
-                                        <NavHashLink
-                                            smooth
-                                            className='d-block'
-                                            onClick={handleClose}
-                                            to='/services#analytics'>
-                                            Analytics
-                                        </NavHashLink>
-                                    </NavDropdown.Item>
-
-                                    <NavDropdown.Item className=''>
-                                        <NavHashLink
-                                            smooth
-                                            className='d-block'
-                                            onClick={handleClose}
-                                            to='/services#design'>
-                                            Design
-                                        </NavHashLink>
-                                    </NavDropdown.Item>
-                                </NavDropdown>
-
-                                <NavDropdown
-                                    title='Services'
-                                    onClick={(e) => navigate('/services')}
-                                    to='/services'
-                                    id='nav-dropdown'
-                                    className='px-lg-4 px-md-3 d-md-none d-block'>
-                                    <NavDropdown.Item className=''>
-                                        <NavHashLink
-                                            smooth
-                                            className='d-block'
-                                            onClick={handleClose}
-                                            to='/services#learning'>
-                                            Learning
-                                        </NavHashLink>
-                                    </NavDropdown.Item>
-
-                                    <NavDropdown.Item className=''>
-                                        <NavHashLink
-                                            smooth
-                                            className='d-block'
-                                            onClick={handleClose}
-                                            to='/services#strategy'>
-                                            Strategy
-                                        </NavHashLink>
-                                    </NavDropdown.Item>
-
-                                    <NavDropdown.Item className=''>
-                                        <NavHashLink
-                                            smooth
-                                            className='d-block'
-                                            onClick={handleClose}
-                                            to='/services#advisory'>
-                                            Advisory / Coaching
-                                        </NavHashLink>
-                                    </NavDropdown.Item>
-
-                                    <NavDropdown.Item className=''>
-                                        <NavHashLink
-                                            smooth
-                                            className='d-block'
-                                            onClick={handleClose}
-                                            to='/services#fundraising'>
-                                            Fundraising
-                                        </NavHashLink>
-                                    </NavDropdown.Item>
-
-                                    <NavDropdown.Item className=''>
-                                        <NavHashLink
-                                            smooth
-                                            className='d-block'
-                                            onClick={handleClose}
-                                            to='/services#analytics'>
-                                            Analytics
-                                        </NavHashLink>
-                                    </NavDropdown.Item>
-
-                                    <NavDropdown.Item className=''>
-                                        <NavHashLink
-                                            smooth
-                                            className='d-block'
-                                            onClick={handleClose}
-                                            to='/services#design'>
-                                            Design
-                                        </NavHashLink>
-                                    </NavDropdown.Item>
-                                </NavDropdown>
-
-                                <Nav.Item className='team-link px-lg-4 px-md-3'>
-                                    <HashLink
+                            <NavDropdown
+                                title='Services'
+                                onClick={(e) => navigate('/services')}
+                                to='/services'
+                                id='nav-dropdown'
+                                className='pb-lg-4 pb-3'>
+                                <NavDropdown.Item className=''>
+                                    <NavHashLink
                                         smooth
-                                        onClick={handleClose}
-                                        to='/about#team'>
-                                        Team
-                                    </HashLink>
-                                </Nav.Item>
-                                <Nav.Item className='px-lg-4 px-md-3'>
-                                    <NavLink onClick={handleClose} to='/blog'>
-                                        Blog
-                                    </NavLink>
-                                </Nav.Item>
-                                <Nav.Item className='ps-lg-4 ps-md-3 d-md-block d-none'>
-                                    <NavLink
-                                        onClick={handleClose}
-                                        to='/contact'>
-                                        Contact
-                                    </NavLink>
-                                </Nav.Item>
-                            </Nav>
-                            <Button
-                                as='a'
-                                href='/contact'
-                                className='mt-4 d-md-none d-block contact-btn'>
-                                Schedule Consultation
-                            </Button>
-                        </Offcanvas.Body>
-                    </Navbar.Offcanvas>
+                                        className='d-block'
+                                        to='/services#learning'>
+                                        Learning
+                                    </NavHashLink>
+                                </NavDropdown.Item>
+
+                                <NavDropdown.Item className=''>
+                                    <NavHashLink
+                                        smooth
+                                        className='d-block'
+                                        to='/services#strategy'>
+                                        Strategy
+                                    </NavHashLink>
+                                </NavDropdown.Item>
+
+                                <NavDropdown.Item className=''>
+                                    <NavHashLink
+                                        smooth
+                                        className='d-block'
+                                        to='/services#advisory'>
+                                        Advisory / Coaching
+                                    </NavHashLink>
+                                </NavDropdown.Item>
+
+                                <NavDropdown.Item className=''>
+                                    <NavHashLink
+                                        smooth
+                                        className='d-block'
+                                        to='/services#fundraising'>
+                                        Fundraising
+                                    </NavHashLink>
+                                </NavDropdown.Item>
+
+                                <NavDropdown.Item className=''>
+                                    <NavHashLink
+                                        smooth
+                                        className='d-block'
+                                        to='/services#analytics'>
+                                        Analytics
+                                    </NavHashLink>
+                                </NavDropdown.Item>
+
+                                <NavDropdown.Item className=''>
+                                    <NavHashLink
+                                        smooth
+                                        className='d-block'
+                                        to='/services#design'>
+                                        Design
+                                    </NavHashLink>
+                                </NavDropdown.Item>
+                            </NavDropdown>
+
+                            <Nav.Item className='team-link pb-lg-4 pb-3'>
+                                <HashLink smooth to='/about#team'>
+                                    Team
+                                </HashLink>
+                            </Nav.Item>
+                            <Nav.Item className='pb-lg-4 pb-3'>
+                                <NavLink to='/blog'>Blog</NavLink>
+                            </Nav.Item>
+                            <Nav.Item className='pb-lg-4 pb-3 d-md-block d-none'>
+                                <NavLink to='/contact'>Contact</NavLink>
+                            </Nav.Item>
+                        </Nav>
+                        <Button
+                            as='a'
+                            href='/contact'
+                            className='d-md-none d-block contact-btn text-white'>
+                            Schedule Consultation
+                        </Button>
+                    </Navbar.Collapse>
                 </Container>
             </Navbar>
         </header>
